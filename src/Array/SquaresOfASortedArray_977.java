fded34360db8efc23fcf6dd2f85b37a68dc1c0df
Lenny <ly2484579947@gmail.com>
2024-12-11 20:54:16 +0800

    feat: day03_squaresofsortedarray_977

---
+++ src/Array/SquaresOfASortedArray_977.java
@@ -0,0 +1,20 @@
+package Array;
+
+public class SquaresOfASortedArray_977 {
+  public static int[] sortedSquares(int[] nums) {
+//   由于单调性,能确定最大值的位置, 要么在最左边要么在最右边
+    int l = 0, r = nums.length - 1;
+    int[] res = new int[nums.length];
+    int j = nums.length - 1;
+    while (l <= r) {
+      if (nums[l] * nums[l] > nums[r] * nums[r]) {
+        res[j--] = nums[l] * nums[l++];
+      }else {
+        res[j--] = nums[r] * nums[r--];
+      }
+    }
+    return res;
+  }
+
+}
+
