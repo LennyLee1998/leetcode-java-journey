498adba187b5fab421ed9444e30092998660f0c6
Lenny <ly2484579947@gmail.com>
2025-01-01 19:45:31 +0800

    feat: day11_reverseLinkedList_206

---
+++ src/LinkedList/ReverseLinkedList_206.java
@@ -0,0 +1,33 @@
+package LinkedList;
+//https://leetcode.com/problems/reverse-linked-list/
+//https://github.com/youngyangyang04/leetcode-master/blob/master/problems/0206.%E7%BF%BB%E8%BD%AC%E9%93%BE%E8%A1%A8.md
+public class ReverseLinkedList_206 {
+  public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+      this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+      this.val = val;
+      this.next = next;
+    }
+  }
+  public ListNode reverseList(ListNode head) {
+    ListNode prev = null;
+    ListNode cur = head;
+    ListNode next = null;
+    while (cur != null) {
+      next = cur.next;
+      cur.next = prev;
+      prev = cur;
+      cur = next;
+    }
+    return prev;
+  }
+}
