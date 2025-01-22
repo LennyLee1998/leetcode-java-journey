7d8e44223a0b29ae1c851fae14f8d4cda7f4a1c3
Lenny <ly2484579947@gmail.com>
2025-01-22 13:18:08 +0800

    chore: add comments to improve `ValidAnagram_242`

--- src/HashTable/ValidAnagram_242.java
+++ src/HashTable/ValidAnagram_242.java
@@ -1,5 +1,9 @@
 package HashTable;
-
+//给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
+//示例 1: 输入: s = "anagram", t = "nagaram" 输出: true
+//示例 2: 输入: s = "rat", t = "car" 输出: false
+//说明: 你可以假设字符串只包含小写字母。
+//https://github.com/youngyangyang04/leetcode-master/blob/master/problems/0242.%E6%9C%89%E6%95%88%E7%9A%84%E5%AD%97%E6%AF%8D%E5%BC%82%E4%BD%8D%E8%AF%8D.md
 public class ValidAnagram_242 {
   public boolean isAnagram(String s, String t) {
 
